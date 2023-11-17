5db9d31a25fd3edf8bda3506bfe2efaf21ca4b3b
Antibody <94369215+Antibodyy@users.noreply.github.com>
2023-11-17 15:23:47 +0530

    Twist mux and nav2 implemented. Need to add params files to repo

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -15,9 +15,7 @@ def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='osiris_ros' #<--- CHANGE ME
+    package_name='osiris_ros' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
